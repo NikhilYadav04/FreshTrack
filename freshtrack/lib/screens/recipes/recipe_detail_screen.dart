f4de86295aa0e2e2d24d9c602a92b1c02cc4e886
NikhilYadav04 <byadav1723@gmail.com>
2025-01-24 17:45:13 +0530

    Changes in doc

--- freshtrack/lib/screens/recipes/recipe_detail_screen.dart
+++ freshtrack/lib/screens/recipes/recipe_detail_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:freshtrack/GetX_Controllers/saved_recipe_controller.dart';
@@ -11,7 +12,8 @@ import 'package:freshtrack/styling/sizeConfig.dart';
 
 // ignore: must_be_immutable
 class RecipeDetailScreen extends StatelessWidget {
-  final SavedRecipeController savedRecipeController = Get.put(SavedRecipeController());
+  final SavedRecipeController savedRecipeController =
+      Get.put(SavedRecipeController());
   String title;
   String ingredients;
   String image;
@@ -36,22 +38,20 @@ class RecipeDetailScreen extends StatelessWidget {
             icon: Icon(
               Icons.arrow_back_ios,
               color: Colors.white,
-              size: 3.79214*SizeConfig.heightMultiplier,
-            )
-            ),
-          actions: [
-            IconButton(
-            onPressed: () {
-              final email = FirebaseAuth.instance.currentUser!.email!;
-              savedRecipeController.deleteItem(context,email , title, making);
-            },
-            icon: Icon(
-              Icons.delete,
-              color: Colors.white,
-              size: 3.79214*SizeConfig.heightMultiplier,
-            )
-            ),
-          ],
+              size: 3.79214 * SizeConfig.heightMultiplier,
+            )),
+        actions: [
+          IconButton(
+              onPressed: () {
+                final email = FirebaseAuth.instance.currentUser!.email!;
+                savedRecipeController.deleteItem(context, email, title, making);
+              },
+              icon: Icon(
+                Icons.delete,
+                color: Colors.white,
+                size: 3.79214 * SizeConfig.heightMultiplier,
+              )),
+        ],
         centerTitle: true,
         backgroundColor: Colours.Green,
         elevation: 15,
@@ -68,77 +68,84 @@ class RecipeDetailScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 2.9017*SizeConfig.widthMultiplier),
+          padding: EdgeInsets.symmetric(
+              horizontal: 2.9017 * SizeConfig.widthMultiplier),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: 2.63343*SizeConfig.heightMultiplier,
+                height: 2.63343 * SizeConfig.heightMultiplier,
               ),
               ClipRRect(
-                  borderRadius: BorderRadius.circular(1.053*SizeConfig.heightMultiplier),
-                  child: Image.network(
-                    image,
-                    height: 30.02117*SizeConfig.heightMultiplier,
-                    width: 94.866*SizeConfig.widthMultiplier,
+                  borderRadius: BorderRadius.circular(
+                      1.053 * SizeConfig.heightMultiplier),
+                  child: CachedNetworkImage(
+                    imageUrl: image,
+                    height: 30.02117 * SizeConfig.heightMultiplier,
+                    width: 94.866 * SizeConfig.widthMultiplier,
+                    placeholder: (context, url) => Center(
+                        child: CircularProgressIndicator(
+                      color: Colours.Green,
+                    )),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
                   )),
               SizedBox(
-                height: 2.1067*SizeConfig.heightMultiplier,
+                height: 2.1067 * SizeConfig.heightMultiplier,
               ),
               Text(
                 title,
                 style: style.copyWith(
-                    fontSize: 3.581473*SizeConfig.heightMultiplier,
+                    fontSize: 3.581473 * SizeConfig.heightMultiplier,
                     fontFamily: "Poppins",
                     color: Colors.grey.shade900,
                     fontWeight: FontWeight.w800),
               ),
               SizedBox(
-                height:  2.1067*SizeConfig.heightMultiplier,
+                height: 2.1067 * SizeConfig.heightMultiplier,
               ),
               Text(
                 "Ingredients Used :",
                 style: style.copyWith(
-                    fontSize: 2.949448*SizeConfig.heightMultiplier,
+                    fontSize: 2.949448 * SizeConfig.heightMultiplier,
                     fontFamily: "Poppins",
                     color: Colors.green.shade900,
                     fontWeight: FontWeight.w800),
               ),
               SizedBox(
-                height: 1.58006*SizeConfig.heightMultiplier,
+                height: 1.58006 * SizeConfig.heightMultiplier,
               ),
               Text(
                 ingredients,
                 style: style.copyWith(
-                    fontSize: 2.528098*SizeConfig.heightMultiplier,
+                    fontSize: 2.528098 * SizeConfig.heightMultiplier,
                     fontFamily: "Poppins",
                     color: Colors.grey.shade800,
                     fontWeight: FontWeight.w800),
               ),
               SizedBox(
-                height:   2.1067*SizeConfig.heightMultiplier,
+                height: 2.1067 * SizeConfig.heightMultiplier,
               ),
               Text(
                 "How To Prepare? :",
                 style: style.copyWith(
-                    fontSize: 2.949448*SizeConfig.heightMultiplier,
+                    fontSize: 2.949448 * SizeConfig.heightMultiplier,
                     fontFamily: "Poppins",
                     color: Colors.green.shade900,
                     fontWeight: FontWeight.w800),
               ),
               SizedBox(
-                height: 1.58006*SizeConfig.heightMultiplier,
+                height: 1.58006 * SizeConfig.heightMultiplier,
               ),
               Text(
                 making,
                 style: style.copyWith(
-                    fontSize: 2.31742*SizeConfig.heightMultiplier,
+                    fontSize: 2.31742 * SizeConfig.heightMultiplier,
                     fontFamily: "Poppins",
                     color: Colors.grey.shade800,
                     fontWeight: FontWeight.w800),
               ),
-               SizedBox(
-                height: 2.63343*SizeConfig.heightMultiplier,
+              SizedBox(
+                height: 2.63343 * SizeConfig.heightMultiplier,
               ),
             ],
           ),
